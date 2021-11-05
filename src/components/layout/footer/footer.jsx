be33d3856e02a52d7aa5dcc16634e8b640b813bc
Anton Petrov <52886942+webdirection-dev@users.noreply.github.com>
2021-11-05 22:03:22 +0300

    ADD
    Added links to the task and sources

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright
-                    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/webdirection-dev/templater-spa">Repo</a>
                 </div>
             </div>
         </footer>
